ea3dc3a9c831bb7a501a4acf6d8f11ab67388935
mouthzipper <ramos.jeromeh@gmail.com>
2015-03-29 19:12:36 +0800

    add server structure

--- app.js
+++ app.js
@@ -2,11 +2,17 @@ var express = require('express');
 var http = require('http');
 var mongoose = require( 'mongoose' );
 
+require( './src/server/models/posts' );
+
 // connect to DB
 mongoose.connect( 'mongodb://localhost/mouthzipperNews' );
 
 var app = express();
 
 app.use(express.static(__dirname + '/public/client'));
+
+app.get( '/post', function( req, res ) {
+
+} );
 
 http.createServer(app).listen(3000);
