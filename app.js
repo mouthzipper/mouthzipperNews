e698670e12faa742f46ba15583552dfd6de6ae60
mouthzipper <ramos.jeromeh@gmail.com>
2015-03-29 17:18:28 +0800

    add mongoose

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var http = require('http');
+var mongoose = require( 'mongoose' );
+
+// connect to DB
+mongoose.connect( 'mongodb://localhost/mouthzipperNews' );
+
 var app = express();
 
 app.use(express.static(__dirname + '/public/client'));
